7fd75cec7757e95127c9b72abf7f3473974ca42e
Filiph <83348675+Flippen69@users.noreply.github.com>
2023-03-09 15:44:55 +0100

    "All_posts" sidan är klar.
    Lite små fix

--- projekt/app.rb
+++ projekt/app.rb
@@ -2,10 +2,17 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
+require_relative 'authenticationMiddleware.rb'
 enable :sessions
+
+# use AuthMiddleware
 
 get('/') do
     slim(:register)
+end
+
+get('/register') do
+  slim(:register)
 end
 
 get('/showlogin') do
@@ -20,6 +27,11 @@ get('/posts') do
     slim(:"posts/index", locals:{posts:result})
 end
 
+get('/logout') do 
+  session[:id] = nil
+  redirect("/")
+end
+
 post('/login') do
     username = params[:username]
     password = params[:password]
@@ -30,6 +42,7 @@ post('/login') do
     id = result["id"]
   
     if BCrypt::Password.new(pwdigest) == password
+      
       session[:id] = id
       redirect('/posts')
     else
@@ -37,28 +50,28 @@ post('/login') do
     end
 end
   
-  post('/post') do
+post('/post') do
     post = params[:post]
     id = session[:id]
     db = SQLite3::Database.new('db/db_forum.db')
     db.execute("INSERT INTO posts (content, user_id) VALUES (?,?)",post, id)
     redirect('/posts')
-  end
+end
   
-  post('/post/:id/delete') do
+post('/post/:id/delete') do
     id = params[:id].to_i
     db = SQLite3::Database.new('db/db_forum.db')
     db.execute("DELETE FROM posts WHERE id = ?", id)
     redirect('/posts')
-  end
+end
   
-  post('/post/:id/update') do
+post('/post/:id/update') do
     id = params[:id].to_i
     edit_post = params[:edit_post]
     db = SQLite3::Database.new('db/db_forum.db')
     db.execute("UPDATE posts SET content=? WHERE id=?", edit_post, id)
     redirect('/posts')
-  end
+end
 
   
 post('/users/new') do
@@ -74,4 +87,17 @@ post('/users/new') do
     else
       "lösen mathcade inte"
     end
+end
+
+get('/support') do
+
+  slim(:support)
+end
+
+get('/all_posts') do
+  db = SQLite3::Database.new('db/db_forum.db')
+  db.results_as_hash = true
+  @all_posts = db.execute("SELECT posts.content, users.name FROM posts INNER JOIN users ON posts.user_id = users.id")
+
+  slim(:all_posts)
 end
