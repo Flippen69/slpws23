32bcb6bb5b5dce128d6598c4c69b3676ca2c5346
Filiph <83348675+Flippen69@users.noreply.github.com>
2023-03-17 09:44:43 +0100

    Några nya uppdateringar
    Allt dokumenteras i dokumentationen!

--- projekt/app.rb
+++ projekt/app.rb
@@ -3,6 +3,7 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 require_relative 'authenticationMiddleware.rb'
+require 'pony'
 enable :sessions
 
 # use AuthMiddleware
@@ -19,12 +20,20 @@ get('/showlogin') do
     slim(:login)
 end
 
+# get('/posts') do
+#   id = session[:id].to_i
+#   db = SQLite3::Database.new('db/db_forum.db')
+#   db.results_as_hash = true
+#   result = db.execute("SELECT posts.id, posts.title, posts.content, users.name FROM posts INNER JOIN users ON posts.user_id = users.id WHERE user_id = ?", id)
+#   slim(:"posts/index", locals:{posts:result})
+# end
+
 get('/posts') do
-    id = session[:id].to_i
-    db = SQLite3::Database.new('db/db_forum.db')
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM posts WHERE user_id = ?", id)
-    slim(:"posts/index", locals:{posts:result})
+  id = session[:id].to_i
+  db = SQLite3::Database.new('db/db_forum.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT posts.id, posts.title, posts.content, users.name FROM posts INNER JOIN users ON posts.user_id = users.id WHERE user_id = ?", id)
+  slim(:"posts/index", locals:{posts:result})
 end
 
 get('/logout') do 
@@ -49,14 +58,16 @@ post('/login') do
       "FEL LÖSEN!"
     end
 end
-  
+
 post('/post') do
-    post = params[:post]
-    id = session[:id]
-    db = SQLite3::Database.new('db/db_forum.db')
-    db.execute("INSERT INTO posts (content, user_id) VALUES (?,?)",post, id)
-    redirect('/posts')
+  post_title = params[:post_title]
+  post_content = params[:post]
+  user_id = session[:id]
+  db = SQLite3::Database.new('db/db_forum.db')
+  db.execute("INSERT INTO posts (title, content, user_id) VALUES (?,?,?)", post_title, post_content, user_id)
+  redirect('/posts')
 end
+
   
 post('/post/:id/delete') do
     id = params[:id].to_i
@@ -89,15 +100,42 @@ post('/users/new') do
     end
 end
 
+get('/all_posts') do
+  db = SQLite3::Database.new('db/db_forum.db')
+  post_title = params[:post_title]
+  db.results_as_hash = true
+  # @all_posts = db.execute("SELECT posts.title, posts.content, users.name FROM posts INNER JOIN users ON posts.user_id = users.id")
+  @all_posts = db.execute("SELECT posts.title, posts.content, posts.created_at, users.name FROM posts INNER JOIN users ON posts.user_id = users.id")
+
+  slim(:all_posts)
+end
+
 get('/support') do
 
   slim(:support)
 end
 
-get('/all_posts') do
-  db = SQLite3::Database.new('db/db_forum.db')
-  db.results_as_hash = true
-  @all_posts = db.execute("SELECT posts.content, users.name FROM posts INNER JOIN users ON posts.user_id = users.id")
-
-  slim(:all_posts)
+post('/send_email') do
+    name = params[:name]
+    email = params[:email]
+    message = params[:message]
+  
+    Pony.mail({
+      :to => 'filiph.sooder@gmail.com', # replace with your email address
+      :from => email,
+      :subject => 'New support request from ' + name,
+      :body => message,
+      :via => :smtp,
+      :via_options => {
+        :address => 'smtp.gmail.com', # replace with your email provider's SMTP server
+        :port => '587',
+        :enable_starttls_auto => true,
+        :user_name => 'filiph.sooder@gmail.com', # replace with your email address
+        :password => 'Filiph1971', # replace with your email password
+        :authentication => :plain,
+        :domain => "localhost.localdomain"
+      }
+    })
+  
+    redirect '/support?message=success'
 end
--- projekt/authenticationMiddleware.rb
+++ projekt/authenticationMiddleware.rb
@@ -1,22 +1,22 @@
 
-class AuthMiddleware
-    def initialize(app)
-        @app = app
-    end
+# class AuthMiddleware
+#     def initialize(app)
+#         @app = app
+#     end
 
-    def call(env)
-        path = env["PATH_INFO"]
-        p path
-        user_id = env["rack.session"][":id"]
-        p user_id
+#     def call(env)
+#         path = env["PATH_INFO"]
+#         p path
+#         user_id = env["rack.session"][":id"]
+#         p user_id
 
-        if !path.start_with?("/showlogin") && !path.start_with?("/register") && env['REQUEST_METHOD'] != "POST" && !user_id
-            response = Rack::Response.new
-            response.redirect('/showlogin')
-            response.finish
-        else
-            @app.call(env)
-        end
-    end
-end
+#         if !path.start_with?("/showlogin") && !path.start_with?("/register") && env['REQUEST_METHOD'] != "POST" && !user_id
+#             response = Rack::Response.new
+#             response.redirect('/showlogin')
+#             response.finish
+#         else
+#             @app.call(env)
+#         end
+#     end
+# end
 
